4fab4225808bb7c9a4262cd18d4b7be836ba2e6a
ericraw <17665003393@163.com>
2020-11-01 12:15:19 +0800

    add hashMap method

--- src/simple100/SumForArray.java
+++ src/simple100/SumForArray.java
@@ -1,5 +1,8 @@
 package simple100;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 /**
  * @author EricRaww
  * @create 2020-11-01
@@ -19,6 +22,10 @@ public class SumForArray {
     public static void main(String[] args) {
         int [] arr={2,7,11,15};
         int sum=13;
+        System.out.println(Arrays.toString(twoSumByHashMap(arr, 13)));
+        System.out.println(Arrays.toString(twoSum(arr, 13)));
+
+        //暴力枚举
         for (int i = 0; i <arr.length ; i++) {
             //i==0,j==1
             //i==1,j==2
@@ -32,4 +39,31 @@ public class SumForArray {
             }
         }
     }
+    public static int[] twoSum(int[] nums, int target) {
+        int [] result=new int[2];
+        for (int i = 0; i <nums.length ; i++) {
+            for (int j = i+1; j <nums.length; j++) {
+                int sum1=nums[i]+nums[j];
+                if(sum1==target){
+                    result[0]=i;
+                    result[1]=j;
+                }
+            }
+        }
+       return result;
+    }
+    //哈希表，对于每一个 x，我们首先查询哈希表中是否存在 target - x，然后将 x 插入到哈希表中，即可保证不会让 x
+
+    public static int[] twoSumByHashMap(int [] nums, int target){
+       HashMap<Integer,Integer> hashMap=new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(hashMap.containsKey(target-nums[i])){
+                return new int [] {hashMap.get(target-nums[i]),i};
+            }
+            hashMap.put(nums[i],i);
+        }
+        return new int[0];
+    }
+
+
 }
